b6febbc1bf2c0efb01d0f8aa70a3c5e3f74227a1
charity20 <mcharity980@gmail.com>
2023-06-30 17:44:24 -0700

    changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,15 +15,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nul)\n");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
 		h = h->next;
-		count+= 1;
+		count++;
 	}
 	return (count);
 }
