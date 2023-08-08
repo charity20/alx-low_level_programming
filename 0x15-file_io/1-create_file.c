88c3fbacd6e73a1312b71f220cc973d10e06d573
charity20 <mcharity980@gmail.com>
2023-08-07 18:10:44 -0700

    file redirection / input and output

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int s;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+	s = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (s == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (nletters = 0; text_content[nletters]; nletters++);
+	rwr = write(s, text_content, nletters);
+	if (rwr == -1)
+		return (-1);
+	close(s);
+	return (1);
+}
